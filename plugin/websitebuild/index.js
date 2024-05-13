cff3e1a1110c6bafd883cc4d824e4ad5394d1ad2
Tom Taylor <hello@tomtaylor.name>
2024-05-13 16:11:56 +0100

    Update plugin code

--- plugin/websitebuild/index.js
+++ plugin/websitebuild/index.js
@@ -12,7 +12,8 @@ define(function(require) {
   function renderPublishButton(data) {
     var viewEventName = 'editor' + data.type[0].toUpperCase() + data.type.slice(1);
     Origin.once(viewEventName + ':postRender', function() {
-      const $btn = '<button type="button" class="editor-sidebar-publish action-secondary"><span class="publish">Publish</span><span class="publishing display-none">Publishing...</span></button>';
+      $('.editor-common-sidebar-download').hide();
+      const $btn = $('<button type="button" class="editor-sidebar-publish action-secondary"><span class="publish">Publish</span><span class="publishing display-none">Publishing...</span></button>');
       $('.editor-common-sidebar-download').after($btn);
       $btn.click(handlePublish);
     });
@@ -26,26 +27,20 @@ define(function(require) {
         text: Origin.l10n.t('app.publishdisabled')
       });
     }
-    Helpers.validateCourseContent(Origin.editor.data.course, function(error, isValid) {
-      if(!isValid) {
-        Origin.Notify.alert({ type: 'error', text: error.message });
-        return;
-      }
-      $('.editor-sidebar-publish .publish').addClass('display-none');
-      $('.editor-sidebar-publish .publishing').removeClass('display-none');
+    $('.editor-sidebar-publish .publish').addClass('display-none');
+    $('.editor-sidebar-publish .publishing').removeClass('display-none');
 
-      $.post('/api/adapt/websitebuild' + Origin.editor.data.course.get('_id'), function(jqXHR, textStatus, errorThrown) {
-        $('.editor-sidebar-publish .publishing').addClass('display-none');
-        $('.editor-sidebar-publish .publish').removeClass('display-none');
-        Origin.Notify.alert({
-          type: 'success',
-          text: 'Published successfully'
-        });
-      }).fail(function (jqXHR, textStatus, errorThrown) {
-        Origin.Notify.alert({
-          type: 'error',
-          text: jqXHR.responseText
-        });
+    $.post('/api/adapt/websitebuild/' + Origin.editor.data.course.get('_id'), function(jqXHR, textStatus, errorThrown) {
+      $('.editor-sidebar-publish .publishing').addClass('display-none');
+      $('.editor-sidebar-publish .publish').removeClass('display-none');
+      Origin.Notify.alert({
+        type: 'success',
+        text: Origin.l10n.t('app.publishsuccess')
+      });
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+      Origin.Notify.alert({
+        type: 'error',
+        text: jqXHR.responseText
       });
     });
   }
